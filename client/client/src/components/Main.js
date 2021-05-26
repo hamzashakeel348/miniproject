91445521d8d5f1e72f3ef52ebb5456fc16eea93e
M Hamza Shakeel <55921857+hamzashakeel348@users.noreply.github.com>
2021-05-27 03:31:37 +0500

    Create Main.js

---
+++ client/client/src/components/Main.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Pop from "./Pop";
+const check = () => {
+  console.log("working");
+  <Pop />;
+};
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <a>
+          <Pop head1="Create offer popup" />
+        </a>
+      </div>
+    );
+  }
+}
+
+export default Main;
