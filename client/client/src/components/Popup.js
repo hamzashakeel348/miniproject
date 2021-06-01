fe10283c1dbed54c0c76f2bbcf1c11426bad7992
M Hamza Shakeel <55921857+hamzashakeel348@users.noreply.github.com>
2021-06-01 06:58:44 +0500

    Update Popup.js

--- client/client/src/components/Popup.js
+++ client/client/src/components/Popup.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
+import Popthree from "./pop3";
 
 import { GrClose } from "react-icons/gr";
 import "../stylesheets/Page2.css";
-
+import { BiArrowBack } from "react-icons/bi";
+import Pop from "./Pop";
 import { GoVerified } from "react-icons/go";
 
 const styles = {
@@ -13,12 +15,17 @@ const styles = {
 class Servicespop extends React.Component {
   constructor() {
     super();
+    this.state = {
+      check: false,
+      second_check: false,
+    };
   }
-  tog = () => {
-    document.getElementById("pop").style.display = "block";
-    document.getElementById("Button").style.display = "none";
+  handleClick = () => {
+    this.setState({ check: true });
   };
-
+  handleCheck = () => {
+    this.setState({ second_check: true });
+  };
   clo = () => {
     document.getElementById("pop").style.display = "none";
     document.getElementById("Button").style.display = "block";
@@ -37,49 +44,59 @@ class Servicespop extends React.Component {
   };
 
   render() {
-    return (
-      <div>
-        <div id="pop">
-          <span className="Heading">
-            <h2>Choose card</h2>
-
-            <button onClick={this.clo}>
-              <GrClose />
-            </button>
-            <hr></hr>
-          </span>
-
-          <span id="cell1" onClick={this.check1}>
-            <GoVerified id="Ver1" style={styles} />
-            <span>
-              <img src="https://www.dropbox.com/s/7f5dl373c9hd1ew/MainVisaBlue.png?raw=1" />
-            </span>
-            <p>Visa credit card ending with 2345</p>
-          </span>
-          <span id="cell2" onClick={this.check2}>
-            <GoVerified id="Ver2" style={styles} />
-            <span>
-              <img src="https://www.dropbox.com/s/7f5dl373c9hd1ew/MainVisaBlue.png?raw=1" />
-            </span>
-            <p>Visa credit card ending with 7645</p>
-            {/* <a>
-              <Pop head1="Create offer popup" />
-            </a> */}
-          </span>
-
-          <span id="cell2">
-            <h5>+ Add card</h5>
-          </span>
-          <br></br>
-
-          <button className="button">Pay</button>
+    if (this.state.check) {
+      return (
+        <div>
+          <Pop head=" "></Pop>
         </div>
+      );
+    } else if (this.state.second_check) {
+      return (
+        <div>
+          <Popthree></Popthree>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <div id="pop">
+            <span className="Heading">
+              <BiArrowBack id="arrow" onClick={this.handleClick} />
+              <h2>Choose card</h2>
 
-        <span id="Button" onClick={this.tog}>
-          {this.props.head}
-        </span>
-      </div>
-    );
+              <button onClick={this.clo}>
+                <GrClose />
+              </button>
+              <hr></hr>
+            </span>
+
+            <span id="cell1" onClick={this.check1}>
+              <GoVerified id="Ver1" style={styles} />
+              <span>
+                <img src="https://www.dropbox.com/s/7f5dl373c9hd1ew/MainVisaBlue.png?raw=1" />
+              </span>
+              <p>Visa credit card ending with 2345</p>
+            </span>
+            <span id="cell2" onClick={this.check2}>
+              <GoVerified id="Ver2" style={styles} />
+              <span>
+                <img src="https://www.dropbox.com/s/7f5dl373c9hd1ew/MainVisaBlue.png?raw=1" />
+              </span>
+              <p>Visa credit card ending with 7645</p>
+            </span>
+
+            <span id="cell2">
+              <h5>+ Add card</h5>
+            </span>
+            <br></br>
+
+            <button className="button" onClick={this.handleCheck}>
+              Pay
+            </button>
+          </div>
+        </div>
+      );
+    }
   }
 }
 
