8958643867d014fb0f9acf3d1c227c93def34d85
M Hamza Shakeel <55921857+hamzashakeel348@users.noreply.github.com>
2021-06-02 02:09:29 +0500

    Update Popup.js

--- client/client/src/components/Popup.js
+++ client/client/src/components/Popup.js
@@ -1,18 +1,18 @@
-import React, { useState } from "react";
-import Popthree from "./pop3";
-
-import { GrClose } from "react-icons/gr";
+import React from "react";
 import "../stylesheets/Page2.css";
-import { BiArrowBack } from "react-icons/bi";
+import Popthree from "./pop3";
 import Pop from "./Pop";
+import { BiArrowBack } from "react-icons/bi";
 import { GoVerified } from "react-icons/go";
+import { GrClose } from "react-icons/gr";
 
+//Styled Components for Body Tags
 const styles = {
   display: "inline-block",
   color: "green",
 };
 
-class Servicespop extends React.Component {
+class Poptwo extends React.Component {
   constructor() {
     super();
     this.state = {
@@ -28,10 +28,9 @@ class Servicespop extends React.Component {
   };
   clo = () => {
     document.getElementById("pop").style.display = "none";
-    document.getElementById("Button").style.display = "block";
   };
   check1 = () => {
-    document.getElementById("cell1").style.border = "1.5px solid #3a52a1";
+    document.getElementById("cell1").style.border = "1.5px solid #2955e4";
     document.getElementById("cell2").style.border = "1.5px solid #e0e0e0";
     document.getElementById("Ver1").style.visibility = "visible";
     document.getElementById("Ver2").style.visibility = "hidden";
@@ -40,17 +39,20 @@ class Servicespop extends React.Component {
     document.getElementById("Ver1").style.visibility = "hidden";
     document.getElementById("Ver2").style.visibility = "visible";
     document.getElementById("cell1").style.border = "1.5px solid #e0e0e0";
-    document.getElementById("cell2").style.border = "1.5px solid #3a52a1";
+    document.getElementById("cell2").style.border = "1.5px solid #2955e4";
   };
 
   render() {
+    // checking for the back arrow rendering
     if (this.state.check) {
       return (
         <div>
           <Pop head=" "></Pop>
         </div>
       );
-    } else if (this.state.second_check) {
+    }
+    //Check for the Forward Page Rendering
+    else if (this.state.second_check) {
       return (
         <div>
           <Popthree></Popthree>
@@ -59,38 +61,53 @@ class Servicespop extends React.Component {
     } else {
       return (
         <div>
-          <div id="pop">
+          <div id="pop" data-testid="pop2">
             <span className="Heading">
-              <BiArrowBack id="arrow" onClick={this.handleClick} />
+              {/* Backarrow button to move back to first page */}
+              <BiArrowBack
+                id="arrow"
+                onClick={this.handleClick}
+                data-testid="back"
+              />
               <h2>Choose card</h2>
-
-              <button onClick={this.clo}>
+              {/* exit button to exit the popup */}
+              <button onClick={this.clo} data-testid="exit">
                 <GrClose />
               </button>
               <hr></hr>
             </span>
-
-            <span id="cell1" onClick={this.check1}>
+            {/* First Card Option */}
+            <span id="cell1" onClick={this.check1} data-testid="Cell1">
               <GoVerified id="Ver1" style={styles} />
               <span>
-                <img src="https://www.dropbox.com/s/7f5dl373c9hd1ew/MainVisaBlue.png?raw=1" />
+                <img
+                  src="https://www.dropbox.com/s/7f5dl373c9hd1ew/MainVisaBlue.png?raw=1"
+                  alt="Error"
+                />
               </span>
               <p>Visa credit card ending with 2345</p>
             </span>
-            <span id="cell2" onClick={this.check2}>
+            {/* Second Card option */}
+            <span id="cell2" onClick={this.check2} data-testid="Cell2">
               <GoVerified id="Ver2" style={styles} />
               <span>
-                <img src="https://www.dropbox.com/s/7f5dl373c9hd1ew/MainVisaBlue.png?raw=1" />
+                <img
+                  src="https://www.dropbox.com/s/7f5dl373c9hd1ew/MainVisaBlue.png?raw=1"
+                  alt="Error"
+                />
               </span>
               <p>Visa credit card ending with 7645</p>
             </span>
-
+            {/* Dummy Add Card Button */}
             <span id="cell2">
               <h5>+ Add card</h5>
             </span>
-            <br></br>
-
-            <button className="button" onClick={this.handleCheck}>
+            {/* Payment Button to do payment process but here it do conditional rendering */}
+            <button
+              className="button"
+              data-testid="Pay-Button"
+              onClick={this.handleCheck}
+            >
               Pay
             </button>
           </div>
@@ -100,4 +117,4 @@ class Servicespop extends React.Component {
   }
 }
 
-export default Servicespop;
+export default Poptwo;
